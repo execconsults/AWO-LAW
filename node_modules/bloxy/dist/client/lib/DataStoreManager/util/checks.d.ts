e36e3b5b046888fde1ae928a68586a5104b5b7c8
zlivehe55 <71079664+zlivehe55@users.noreply.github.com>
2024-01-26 16:06:59 -0500

    main

--- node_modules/bloxy/dist/client/lib/DataStoreManager/util/checks.d.ts
+++
@@ -1,5 +0,0 @@
-export declare function checkKey(key: string): boolean;
-export declare function checkPlaceId(placeId: number): boolean;
-export declare function checkName(name: string): boolean;
-export declare function checkScope(scope: string): boolean;
-export declare function checkValue(value: string): boolean;
--- views/utilities/Converter.js
+++
@@ -1,8 +0,0 @@
-module.exports = func => {
-    return (req,res,next) =>{
-        const useramount = req.user.currentcy/10
-        
-        
-
-    }
-}
